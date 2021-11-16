1af3e691923dff615cba4fd80484005a1c291446
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2021-11-16 14:36:33 +0700

    Add login functionality login now can be access as index, formating static call in html, and move static and templates file to correct folder

--- parent_center_app/views.py
+++ parent_center_app/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.contrib.auth import authenticate, login
+
 
 # Create your views here.
+def login_user(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+    else:
+        if request.method == 'POST':
+            username = request.POST.get('username')
+            password = request.POST.get('password')
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect('login_user')
+            else:
+                messages.error(request, 'Username or Password is incorrect')
+        return render(request, 'parent_center_app/login.html', {})
