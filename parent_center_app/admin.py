8b7a4761a815dbffd9e8b2040fcbfb5aadefcebd
sayyidinside <70059117+sayyidinside@users.noreply.github.com>
2021-11-18 20:30:47 +0700

    Create Model mapel and add superuser

--- parent_center_app/admin.py
+++ parent_center_app/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import mapel
 
 # Register your models here.
+admin.site.register(mapel)
--- parent_center_app/models.py
+++ parent_center_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
+
 # Create your models here.
+class mapel(models.Model):
+    id_mapel = models.CharField(max_length=10)
+    nama = models.CharField(max_length=50)
+
+    class jenis_mapel(models.TextChoices):
+        C1 = 'C1'
+        C2 = 'C2'
+        C3 = 'C3'
+
+    jenis = models.CharField(max_length=2,
+                             choices=jenis_mapel.choices,
+                             default=jenis_mapel.C1)
