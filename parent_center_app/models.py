bbc7977f0e63beb70eeb2281670bd391985ffaf4
Ami2112 <asadin.inzaghi90@gmail.com>
2021-11-23 08:21:42 +0700

    apdet

--- parent_center_app/models.py
+++ parent_center_app/models.py
@@ -1,15 +1,18 @@
 from datetime import date
 from django.db import models
-from django.db.models.deletion import CASCADE, SET_NULL
-from django.db.models.fields import EmailField
-from .utils import custom_jadwal, custom_user,custom_absen, custom_adm, custom_guru,custom_kelas, custom_mapel, custom_siswa, custom_spp, custom_tugas
-from datetime import datetime
+from django.db.models.deletion import CASCADE
+from .utils import custom_jadwal, custom_user, custom_absen, custom_adm, custom_guru
+from .utils import custom_kelas, custom_mapel, custom_siswa, custom_spp, custom_tugas
+from django.contrib.auth.models import User
+
+
 # Create your models here.
 class mapel(models.Model):
     id_mapel = models.CharField(max_length=10,
                                 primary_key=True,
                                 unique=True,
-                                default=custom_mapel,editable=False)
+                                default=custom_mapel,
+                                editable=False)
     nama = models.CharField(max_length=50)
 
     class jenis_mapel(models.TextChoices):
@@ -21,15 +24,17 @@ class mapel(models.Model):
                              choices=jenis_mapel.choices,
                              default=jenis_mapel.C1)
 
-    def __str__(self) :
+    def __str__(self):
         return self.nama
 
+
 class Kelas(models.Model):
-    id_kelas =  models.CharField(max_length=10,
+    id_kelas = models.CharField(max_length=10,
                                 primary_key=True,
                                 unique=True,
-                                default=custom_kelas,editable=False
-                                )
+                                default=custom_kelas,
+                                editable=False)
+
     class rentang_kelas(models.TextChoices):
         X = 'X'
         XI = 'XI'
@@ -48,30 +53,35 @@ class Kelas(models.Model):
         TBSM = 'Teknik dan Bisnis Sepeda Motor'
 
     jurusan = models.CharField(max_length=30,
-                             choices=jurusan.choices,
-                             default=jurusan.TITL)
+                               choices=jurusan.choices,
+                               default=jurusan.TITL)
     no_kelas = models.SmallIntegerField(default=1)
-    
-    def __str__(self) :
-        return '%s %s %s' % (self.kelas,self.jurusan,self.no_kelas)
 
-class User(models.Model):
+    def __str__(self):
+        return f'{self.kelas} {self.jurusan} {self.no_kelas}'
+
+
+class Extend_User(models.Model):
+    user = models.OneToOneField(User,
+                                null=True,
+                                on_delete=models.CASCADE)
     id_user = models.CharField(max_length=30,
-                                primary_key=True,
-                                unique=True,
-                                default=custom_user,
-                                editable=False)
-    username = models.CharField(max_length=50)
-    password = models.CharField(max_length=20)
+                               primary_key=True,
+                               unique=True,
+                               default=custom_user,
+                               editable=False)
+
     class user_level(models.TextChoices):
         ADMIN = 'Admin'
         GURU = 'Guru'
-        ORTU = 'OrangTua'
+        ORTU = 'Orang Tua'
     user_level = models.CharField(max_length=10,
-                             choices=user_level.choices,
-                             default=user_level.GURU)
-    def __str__(self) :
-        return '%s | %s ' % (self.user_level,self.username)
+                                  choices=user_level.choices,
+                                  default=user_level.GURU)
+
+    def __str__(self):
+        return f'{self.user_level} | {self.user.username}'
+
 
 class Admin(models.Model):
     id_admin = models.CharField(max_length=30,
@@ -83,30 +93,35 @@ class Admin(models.Model):
     email = models.EmailField(default="example@gmail.com")
     no_tlp = models.CharField(max_length=12)
     alamat = models.TextField(null=True)
-    id_user = models.ForeignKey(User,null=True,on_delete = models.SET_NULL)
-    def __str__(self) :
-        return '%s | %s ' % (self.id_user.username,self.nama)
+    id_user = models.ForeignKey(Extend_User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.id_user.user.username} | {self.nama}'
+
 
 class Guru(models.Model):
     id_guru = models.CharField(max_length=30,
-                                primary_key=True,
-                                unique=True,
-                                default=custom_guru,
-                                editable=False)
+                               primary_key=True,
+                               unique=True,
+                               default=custom_guru,
+                               editable=False)
     nama = models.CharField(max_length=50)
     email = models.EmailField(default="example@gmail.com")
     no_tlp = models.CharField(max_length=12)
     no_induk = models.CharField(max_length=20)
     alamat = models.TextField(null=True)
+
     class jns_kelamin(models.TextChoices):
         P = 'Perempuan'
         L = 'Laki-Laki'
     jns_kelamin = models.CharField(max_length=10,
-                             choices=jns_kelamin.choices,
-                             default=jns_kelamin.L)
-    id_user = models.ForeignKey(User,null=True,on_delete = models.SET_NULL)
-    def __str__(self) :
-        return '%s - %s ' % (self.no_induk,self.nama)
+                                   choices=jns_kelamin.choices,
+                                   default=jns_kelamin.L)
+    id_user = models.ForeignKey(Extend_User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.no_induk} - {self.nama}'
+
 
 class Siswa(models.Model):
     nis = models.CharField(max_length=15, null=True)
@@ -120,17 +135,20 @@ class Siswa(models.Model):
     tpt_lahir = models.CharField(max_length=50)
     tgl_lahir = models.DateField(default=date.today)
     agama = models.CharField(max_length=15)
+
     class jns_kelamin(models.TextChoices):
         P = 'Perempuan'
         L = 'Laki-Laki'
     jns_kelamin = models.CharField(max_length=10,
-                             choices=jns_kelamin.choices,
-                             default=jns_kelamin.L)
-    no_tlp = models.CharField(max_length=13,null=True)
+                                   choices=jns_kelamin.choices,
+                                   default=jns_kelamin.L)
+    no_tlp = models.CharField(max_length=13, null=True)
     alamat = models.TextField(null=True)
-    id_kelas = models.ForeignKey(Kelas,null=True,on_delete = models.SET_NULL)
-    def __str__(self) :
-        return '%s - %s ' % (self.nis,self.nama)
+    id_kelas = models.ForeignKey(Kelas, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.nis} - {self.nama}'
+
 
 class OrangTua(models.Model):
     id_ortu = models.ForeignKey(Siswa, on_delete=CASCADE)
@@ -138,9 +156,11 @@ class OrangTua(models.Model):
     keterangan = models.CharField(max_length=50)
     no_tlp = models.CharField(max_length=13)
     alamat = models.TextField(null=True)
-    id_user = models.ForeignKey(User,null=True,on_delete = models.SET_NULL)
-    def __str__(self) :
-        return '%s | Orangtua %s - %s ' % (self.nama,self.id_ortu.nis, self.id_ortu.nama)
+    id_user = models.ForeignKey(Extend_User, null=True, on_delete=models.SET_NULL)
+
+    def __str__(self):
+        return f'{self.nama} | Orangtua dari {self.id_ortu.nama} - {self.id_ortu.nis}'
+
 
 class Tugas(models.Model):
     id_tugas = models.CharField(max_length=30,
@@ -150,28 +170,30 @@ class Tugas(models.Model):
                                 editable=False)
     id_mapel = models.ForeignKey(mapel, on_delete=CASCADE)
     id_kelas = models.ForeignKey(Kelas, on_delete=CASCADE)
-    id_guru = models.ForeignKey(Guru,null=True,on_delete = models.SET_NULL)
+    id_guru = models.ForeignKey(Guru, null=True, on_delete=models.SET_NULL)
     nama = models.CharField(max_length=100)
     keterangan = models.TextField(null=True)
-    def __str__(self) :
-        return '%s | %s %s %s | %s | %s ' % (self.id_mapel.nama,
-                                            self.id_kelas.kelas,
-                                            self.id_kelas.jurusan,
-                                            self.id_kelas.no_kelas,
-                                            self.nama,self.keterangan )
-                            
+
+    def __str__(self):
+        return f'''{self.id_mapel.nama} |
+                   {self.id_kelas.kelas} {self.id_kelas.jurusan} {self.id_kelas.no_kelas} |
+                   {self.nama} |
+                   {self.keterangan}'''
+
 
 class NilaiTugas(models.Model):
     id_tugas = models.ForeignKey(Tugas, on_delete=CASCADE)
     id_siswa = models.ForeignKey(Siswa, on_delete=CASCADE)
     nilai = models.IntegerField()
-    def __str__(self) :
-        return '%s | %s %s %s | %s | %s | %s' % (self.id_tugas.id_mapel.nama,
-                                            self.id_tugas.id_kelas.kelas,
-                                            self.id_tugas.id_kelas.jurusan,
-                                            self.id_tugas.id_kelas.no_kelas,
-                                            self.id_tugas.nama,
-                                            self.id_siswa.nama,self.nilai)
+
+    def __str__(self):
+        return f'''{self.id_tugas.id_mapel.nama} |
+                   {self.id_tugas.id_kelas.kelas} {self.id_tugas.id_kelas.jurusan}
+                   {self.id_tugas.id_kelas.no_kelas} |
+                   {self.id_tugas.nama} |
+                   {self.id_siswa.nama} |
+                   {self.nilai}'''
+
 
 class Absen(models.Model):
     id_absen = models.CharField(max_length=30,
@@ -181,42 +203,47 @@ class Absen(models.Model):
                                 editable=False)
     id_mapel = models.ForeignKey(mapel, on_delete=CASCADE)
     id_kelas = models.ForeignKey(Kelas, on_delete=CASCADE)
-    id_guru = models.ForeignKey(Guru, null=True,on_delete = models.SET_NULL)
+    id_guru = models.ForeignKey(Guru, null=True, on_delete=models.SET_NULL)
     tgl = models.DateField(default=date.today)
     pertemuan = models.IntegerField()
-    def __str__(self) :
-        return '%s %s %s | %s | Pertemuan ke %s | %s ' % (self.id_kelas.kelas,
-                                            self.id_kelas.jurusan,
-                                            self.id_kelas.no_kelas,
-                                            self.id_mapel.nama,
-                                            self.pertemuan,self.tgl )
+
+    def __str__(self):
+        return f'''{self.id_kelas.kelas} {self.id_kelas.jurusan} {self.id_kelas.no_kelas} |
+                   {self.id_mapel.nama} |
+                   Pertemuan ke {self.pertemuan} |
+                   {self.tgl}'''
+
 
 class DaftarAbsen(models.Model):
     id_absen = models.ForeignKey(Absen, on_delete=CASCADE)
     id_siswa = models.ForeignKey(Siswa, on_delete=CASCADE)
+
     class keterangan(models.TextChoices):
         H = 'Hadir'
         A = 'Alfa'
         I = 'Izin'
         S = 'Sakit'
-        
+
     keterangan = models.CharField(max_length=10,
-                                    choices=keterangan.choices,
-                                    default=keterangan.H)
-    def __str__(self) :
-        return '%s %s %s | %s | Pertemuan ke %s | %s | %s | %s' % (self.id_absen.id_kelas.kelas,
-                                            self.id_absen.id_kelas.jurusan,
-                                            self.id_absen.id_kelas.no_kelas,
-                                            self.id_absen.id_mapel.nama,
-                                            self.id_absen.pertemuan,self.id_absen.tgl,
-                                            self.id_siswa.nama,self.keterangan )
+                                  choices=keterangan.choices,
+                                  default=keterangan.H)
+
+    def __str__(self):
+        return f'''{self.id_absen.id_kelas.kelas} {self.id_absen.id_kelas.no_kelas}
+                   {self.id_absen.id_kelas.jurusan} |
+                   {self.id_absen.id_mapel.nama} |
+                   Pertemuan ke {self.id_absen.pertemuan} |
+                   {self.id_absen.tgl} |
+                   {self.id_siswa.nama} |
+                   {self.keterangan}'''
+
 
 class PembayaranSPP(models.Model):
     id_pembayaran = models.CharField(max_length=30,
-                                    primary_key=True,
-                                    unique=True,
-                                    default=custom_spp,
-                                    editable=False)
+                                     primary_key=True,
+                                     unique=True,
+                                     default=custom_spp,
+                                     editable=False)
     id_siswa = models.ForeignKey(Siswa, on_delete=CASCADE)
     tgl_bayar = models.DateField(default=date.today)
     bulan_ke = models.SmallIntegerField()
@@ -224,15 +251,23 @@ class PembayaranSPP(models.Model):
     thn_ajar = models.IntegerField()
     semester = models.IntegerField()
 
+    def __str__(self):
+        return f'''{self.id_siswa.nama} |
+                   Lunas Rp. {self.jumlah} |
+                   Tahun ajar {self.thn_ajar} |
+                   Semester {self.semester}'''
+
+
 class Jadwal(models.Model):
     id_jadwal = models.CharField(max_length=30,
-                                primary_key=True,
-                                unique=True,
-                                default=custom_jadwal,
-                                editable=False)
+                                 primary_key=True,
+                                 unique=True,
+                                 default=custom_jadwal,
+                                 editable=False)
     id_kelas = models.ForeignKey(Kelas, on_delete=CASCADE)
     id_mapel = models.ForeignKey(mapel, on_delete=CASCADE)
-    id_guru = models.ForeignKey(Guru,null=True, on_delete=models.SET_NULL)
+    id_guru = models.ForeignKey(Guru, null=True, on_delete=models.SET_NULL)
+
     class hari(models.TextChoices):
         SENIN = 'Senin'
         SELASA = 'Selasa'
@@ -244,9 +279,9 @@ class Jadwal(models.Model):
                             default=hari.SENIN)
     jumlah_jam = models.IntegerField()
     mapel_ke = models.IntegerField()
-    def __str__(self) :
-        return '%s %s %s | Hari %s | Jam Ke %s | Guru %s' % (self.id_kelas.kelas,
-                                                    self.id_kelas.jurusan,
-                                                    self.id_kelas.no_kelas,
-                                                    self.hari, self.mapel_ke, self.id_guru.nama)
-                            
+
+    def __str__(self):
+        return f'''{self.id_kelas.kelas} {self.id_kelas.jurusan} {self.id_kelas.no_kelas} |
+                   Hari {self.hari} |
+                   Jam Ke {self.mapel_ke} |
+                   Guru {self.id_guru.nama}'''
